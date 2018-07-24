a214185f09e6a9d09f7ee0d360c72e4dfcd0f51a
Dania801 <ssansnnom07@gmail.com>
2018-07-24 14:52:25 +0300

    User authentication

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import auth from './routes/auth';
 
+dotenv.config();
 const app = express();
+var dbURI = 'mongodb://localhost/bookworm';
+mongoose.connect(dbURI, { useNewUrlParser: true });
+mongoose.connection.on('connected' , function(){
+    console.log('Mongoose connected to ' + dbURI) ;
+});
+  
+app.use(bodyParser.json());
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: {global: "Invalid credentials! "}});
-})
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
